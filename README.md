d90a968459515442f1511a2950e0af29ea165834
张流欢 <zhangliuhuan@chaintor.com>
2023-05-08 18:05:57 +0800

    完成部分k线图

---
+++ README.md
@@ -0,0 +1,11 @@
+## 基于egui的wasm k线图
+
+### 构建工具
+**`wasm-pack`：** [这里下载](https://rustwasm.github.io/wasm-pack/)。
+
+### 编译运行
+```shell
+yarn
+yarn dev
+```
+
--- src/kline/mod.rs
+++ src/kline/mod.rs
@@ -1,8 +1,9 @@
 use egui::{
-    plot::{BoxElem, BoxPlot, BoxSpread, Plot, PlotBounds, VLine, HLine},
-    Color32, Context, PointerButton, Pos2, Stroke, Ui, Vec2,
+    plot::{Bar, BarChart, BoxElem, BoxPlot, BoxSpread, HLine, Plot, PlotBounds, VLine},
+    Color32, Context, PointerButton, Pos2, Response, Stroke, Ui, Vec2,
 };
 use serde::Serialize;
+use web_sys::console;
 
 use self::utils::{datetime_to_timestamp, timestamp_to_datetime};
 
@@ -42,6 +43,10 @@ pub struct KLine {
     drag_x_move: f32,
     /// 两个蜡烛图的x轴距离
     half_distance: f64,
+    /// 当前帧是否双击蜡烛图
+    is_candle_double_click: bool,
+    /// 当前帧是否双击成交量图
+    is_volume_double_click: bool,
 }
 
 impl Default for KLine {
@@ -58,6 +63,8 @@ impl Default for KLine {
             },
             drag_x_move: 0.0,
             half_distance: 30.0,
+            is_candle_double_click: false,
+            is_volume_double_click: false,
         }
     }
 }
@@ -69,10 +76,10 @@ impl KLine {
         self.size = clip_rect.size();
     }
 
-    /// 将新的k线数据转换为蜡烛图的数据
-    fn set_candles(&mut self) -> Vec<BoxElem> {
+    /// 将新的k线数据转换为蜡烛图和成交量图的数据
+    fn set_candles(&mut self) -> (Vec<BoxElem>, Vec<Bar>) {
         self.y_range_init();
-        let boxs = self
+        let boxs_bars = self
             .candles
             .iter()
             .map(|candle| {
@@ -87,16 +94,28 @@ impl KLine {
                     (candle.open, candle.close, Color32::RED)
                 };
                 let median = (quartile1 + quartile3) / 2.0;
-                BoxElem::new(
+                let box_elem = BoxElem::new(
                     x,
                     BoxSpread::new(candle.low, quartile1, median, quartile3, candle.high),
                 )
                 .whisker_width(0.0)
                 .fill(color)
-                .stroke(Stroke::new(1.0, color))
+                .stroke(Stroke::new(1.0, color));
+                let bar = Bar::new(x, candle.volume)
+                    .fill(color)
+                    .stroke(Stroke::new(1.0, color));
+                (box_elem, bar)
             })
+            .collect::<Vec<(BoxElem, Bar)>>();
+        let boxs = boxs_bars
+            .iter()
+            .map(|item| item.0.to_owned())
             .collect::<Vec<BoxElem>>();
-        boxs
+        let bars = boxs_bars
+            .iter()
+            .map(|item| item.1.to_owned())
+            .collect::<Vec<Bar>>();
+        (boxs, bars)
     }
 
     /// 初始化y_range
@@ -119,10 +138,10 @@ impl KLine {
     }
 
     /// 创建k线图
-    fn draw_kline(&mut self, ui: &mut Ui, candles: Vec<BoxElem>, ctx: &Context) {
-        let response = Plot::new("kline")
+    fn draw_kline(&mut self, ui: &mut Ui, candles: &Vec<BoxElem>, ctx: &Context) -> Response {
+        Plot::new("kline")
             .width(self.size.x - 16.0)
-            .height(self.size.y - 16.0)
+            .height((self.size.y - 16.0) * 0.6)
             .allow_scroll(false)
             .allow_drag(false)
             .x_axis_formatter(|x, _r| {
@@ -140,11 +159,13 @@ impl KLine {
                     x: self.drag_x_move,
                     y: 0.0,
                 });
+                // if self.is_candle_double_click {
                 let bounds = plot_ui.plot_bounds();
                 self.x_range = AxisRange {
                     min: bounds.min()[0],
                     max: bounds.max()[0],
                 };
+                // }
                 self.add_space_y();
                 let plot_bounds: PlotBounds = PlotBounds::from_min_max(
                     [self.x_range.min, self.y_range.min],
@@ -159,7 +180,8 @@ impl KLine {
                         plot_ui.hline(HLine::new(plot_point.y).color(Color32::BLACK));
                         plot_ui.vline(VLine::new(plot_point.x).color(Color32::BLACK));
                         if let Some(candle_boxelme) = candles.iter().find(|p| {
-                            plot_point.x - self.half_distance < p.argument && plot_point.x + self.half_distance > p.argument
+                            plot_point.x - self.half_distance < p.argument
+                                && plot_point.x + self.half_distance > p.argument
                         }) {
                             if let Some(candle) = self.candles.iter().find(|p| {
                                 datetime_to_timestamp(&p.datetime) == candle_boxelme.argument
@@ -177,12 +199,75 @@ impl KLine {
                     }
                 }
             })
-            .response;
-        if response.dragged_by(PointerButton::Primary) {
-            self.drag_x_move = -response.drag_delta().x;
-        } else {
-            self.drag_x_move = 0.0;
-        }
+            .response
+    }
+
+    /// 创建成交量图
+    fn draw_volume(
+        &mut self,
+        ui: &mut Ui,
+        bars: &Vec<Bar>,
+        candles: &Vec<BoxElem>,
+        ctx: &Context,
+    ) -> Response {
+        Plot::new("kline_draw")
+            .width(self.size.x - 16.0)
+            .height((self.size.y - 16.0) * 0.4)
+            .allow_scroll(false)
+            .allow_drag(false)
+            .x_axis_formatter(|x, _r| {
+                if x % 60.0 != 0.0 {
+                    String::new()
+                } else {
+                    timestamp_to_datetime(x)
+                }
+            })
+            .show_y(false)
+            .show_x(false)
+            .label_formatter(|_name, _value| String::new())
+            .show(ui, |plot_ui| {
+                plot_ui.translate_bounds(Vec2 {
+                    x: self.drag_x_move,
+                    y: 0.0,
+                });
+                // let bounds = plot_ui.plot_bounds();
+                // self.x_range = AxisRange {
+                //     min: bounds.min()[0],
+                //     max: bounds.max()[0],
+                // };
+                let plot_bounds: PlotBounds = PlotBounds::from_min_max(
+                    [self.x_range.min, 0.0],
+                    [self.x_range.max, self.y_range.max],
+                );
+                plot_ui.set_plot_bounds(plot_bounds);
+                let chart = BarChart::new(bars.to_owned());
+                plot_ui.bar_chart(chart);
+
+                if plot_ui.plot_hovered() {
+                    if let Some(plot_point) = plot_ui.pointer_coordinate() {
+                        plot_ui.hline(HLine::new(plot_point.y).color(Color32::BLACK));
+                        plot_ui.vline(VLine::new(plot_point.x).color(Color32::BLACK));
+                        if let Some(candle_boxelme) = candles.iter().find(|p| {
+                            plot_point.x - self.half_distance < p.argument
+                                && plot_point.x + self.half_distance > p.argument
+                        }) {
+                            if let Some(candle) = self.candles.iter().find(|p| {
+                                datetime_to_timestamp(&p.datetime) == candle_boxelme.argument
+                            }) {
+                                egui::show_tooltip(ctx, egui::Id::new("tooltip"), |ui| {
+                                    ui.label(format!("日期: {}", candle.datetime));
+                                    ui.label(format!("开盘: {}", candle.open));
+                                    ui.label(format!("最高: {}", candle.high));
+                                    ui.label(format!("最低: {}", candle.low));
+                                    ui.label(format!("收盘: {}", candle.close));
+                                    ui.label(format!("数量: {}", candle.volume));
+                                });
+                            };
+                        };
+                    }
+                }
+            })
+            .response
     }
 
     pub fn show(&mut self, ui: &mut Ui, ctx: &Context) {
@@ -194,7 +279,7 @@ impl KLine {
                 close: 3206.32,
                 high: 3444.32,
                 low: 3103.32,
-                volume: 3206.0,
+                volume: 2206.0,
                 datetime: "2023-04-20 16:10".to_string(),
             },
             Candles {
@@ -210,7 +295,7 @@ impl KLine {
                 close: 3406.32,
                 high: 3456.32,
                 low: 3206.32,
-                volume: 3206.0,
+                volume: 1206.0,
                 datetime: "2023-04-20 16:12".to_string(),
             },
             Candles {
@@ -218,7 +303,7 @@ impl KLine {
                 close: 2506.32,
                 high: 2606.32,
                 low: 2406.32,
-                volume: 3206.0,
+                volume: 2606.0,
                 datetime: "2023-04-20 16:13".to_string(),
             },
             Candles {
@@ -226,12 +311,35 @@ impl KLine {
                 close: 3306.32,
                 high: 3406.32,
                 low: 3006.32,
-                volume: 3206.0,
+                volume: 3506.0,
                 datetime: "2023-04-20 16:14".to_string(),
             },
         ];
-        let candles = self.set_candles();
+        let (candles, bars) = self.set_candles();
         all_candles = candles;
-        self.draw_kline(ui, all_candles, ctx);
+        let candle_response = self.draw_kline(ui, &all_candles, ctx);
+        let volume_response = self.draw_volume(ui, &bars, &all_candles, ctx);
+
+        // 拖动其中一个时，两个一起移动
+        if candle_response.dragged_by(PointerButton::Primary)
+            || volume_response.dragged_by(PointerButton::Primary)
+        {
+            self.drag_x_move = if candle_response.dragged() {
+                -candle_response.drag_delta().x
+            } else {
+                -volume_response.drag_delta().x
+            }
+        } else {
+            self.drag_x_move = 0.0;
+        }
+
+        // if candle_response.double_clicked_by(PointerButton::Primary) {
+        //     self.is_candle_double_click = true;
+        // } else if volume_response.double_clicked_by(PointerButton::Primary) {
+        //     self.is_volume_double_click = true;
+        // } else {
+        //     self.is_candle_double_click = false;
+        //     self.is_volume_double_click = false;
+        // }
     }
 }
